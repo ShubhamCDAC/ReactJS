fd2704b4106c7b668cf5f5271f6a563221204071
SnorlaX <103239836+ShubhamCDAC@users.noreply.github.com>
2023-12-30 21:41:51 +0530

    Update AddAndUpdate.jsx

--- project-5/src/components/AddAndUpdate.jsx
+++ project-5/src/components/AddAndUpdate.jsx
@@ -1,43 +1,81 @@
-import { Formik, Form, Field } from "formik";
 import Modal from "./Modal";
-import { addDoc, collection } from "firebase/firestore";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
 
-const AddAndUpdate = ({ isOpen, onClose }) => {
 
-    const addContact = async(contact) => {
-        try{
+const contactSchemaValidation = Yup.object().shape({
+    name: Yup.string().required("Name is Required."),
+    email: Yup.string().email("Invalid Email").required("Email is Required."),
+})
+
+
+const AddAndUpdate = ({ isOpen, onClose, isUpdate, contacts }) => {
+
+    const addContact = async (contacts) => {
+        try {
             const contactRef = collection(db, "contacts");
-            await addDoc(contactRef, contact);
-        }catch(error){
+            await addDoc(contactRef, contacts);
+            toast.success("Contact Added Successfully");
+            onClose();
+        } catch (error) {
             console.log(error);
         }
     }
+
+    const updateContact = async (contacts, id) => {
+        try {
+            const contactRef = doc(db, "contacts", id);
+            await updateDoc(contactRef, contacts);
+            toast.success("Contact Updated Successfully");
+            onClose();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div>
             <Modal isOpen={isOpen} onClose={onClose}>
-                <Formik 
-                    initialValues={{
-                        name:"",
-                        email:"",
+                <Formik
+                    // formik only supports yup
+                    validationSchema={contactSchemaValidation}
+                    initialValues={isUpdate ? {
+                        name: contacts.name,
+                        email: contacts.email,
+                    } : {
+                        name: "",
+                        email: "",
                     }}
 
-                    onSubmit = {(values) => {
+
+                    onSubmit={(values) => {
                         console.log(values);
-                        addContact(values);
+
+                        isUpdate ?
+                            updateContact(values, contacts.id) :
+                            addContact(values);
                     }}
                 >
                     <Form className="flex flex-col gap-4 ">
                         <div className="flex flex-col gap-1">
                             <label htmlFor="name">Name</label>
                             <Field name="name" className="h-10 border" ></Field>
+                            <div className="text-red-500 text-xs ">
+                                <ErrorMessage name="name" />
+                            </div>
                         </div>
                         <div className="flex flex-col gap-1">
                             <label htmlFor="email">Email</label>
                             <Field name="email" className="h-10 border" ></Field>
+                            <div className="text-red-500 text-xs ">
+                                <ErrorMessage name="email" />
+                            </div>
                         </div>
 
-                        <button className="bg-orange px-3 py-1.5 border self-end" >Add Contact</button>
+                        <button className="bg-orange px-3 py-1.5  self-end hover:text-white rounded-lg " > {isUpdate ? "Update" : "Add"} Contact</button>
                     </Form>
                 </Formik>
             </Modal>
