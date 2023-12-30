4852bd43b401cf1326430a2337cea026e1db9e1e
SnorlaX <103239836+ShubhamCDAC@users.noreply.github.com>
2023-12-30 21:43:35 +0530

    Update Modal.jsx

--- project-5/src/components/Modal.jsx
+++ project-5/src/components/Modal.jsx
@@ -2,12 +2,14 @@ import { AiOutlineClose } from "react-icons/ai"
 import { createPortal } from "react-dom";
 
 const Modal = ({ isOpen, onClose, children }) => {
-    //createPortal is way to render react component outside of DOM hierarchy which helps to not get messy with UI css.
+    // createPortal is way to render react component outside of DOM hierarchy which helps to not get messy with UI css.
     return createPortal(
         <>
             {isOpen && (
-                <>
-                    <div className="relative m-auto z-50 min-h-[200px] max-w-[80%] bg-white p-4">
+                < div
+                    // onClick={onClose}  when i click on this container it will close 
+                    className=" grid place-items-center absolute top-0 z-120 h-screen w-screen backdrop-blur">
+                    <div className="relative m-auto z-50 min-h-[200px] min-w-[80%] bg-white p-4">
                         <div className="flex justify-end">
                             <AiOutlineClose
                                 onClick={onClose}
@@ -15,13 +17,10 @@ const Modal = ({ isOpen, onClose, children }) => {
                         </div>
                         {children}
                     </div>
-                    <div 
-                        onClick={onClose}
-                        className="absolute top-0 z-120 h-screen w-screen backdrop-blur"></div>
-                </>
+                </div>
             )}
         </>
-        ,document.getElementById("modal-root")
+        , document.getElementById("modal-root")
     );
 }
 
