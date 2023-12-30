db23caf75b1f1e026a15767e057ff10964f17369
SnorlaX <103239836+ShubhamCDAC@users.noreply.github.com>
2023-12-30 21:44:37 +0530

    Add files via upload

---
+++ project-5/src/components/NotFoundContact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFoundContact = () => {
+  return (
+    <div className='flex justify-center items-center h-[80vh] gap-4'>
+        <img src='/user.png' alt='user'/>
+        <h3 className='flex items-center text-white text-2xl font-semibold'>Not Found</h3>
+    </div>
+  )
+}
+
+export default NotFoundContact;
